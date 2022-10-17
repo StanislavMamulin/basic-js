efdc8e40d44f60f0927175b4fc503aaf6b6643b4
Stanislav Mamulin <StanislavPM@ya.ru>
2022-10-17 21:20:13 +0500

    feat: add solution to "Transform array" task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Create transformed array based on the control sequences that original
  * array contains
@@ -13,9 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const controls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+
+  let resultArray = [...arr];
+  
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] === '--discard-next') {
+      resultArray[i + 1] = undefined;
+    } else if (arr[i] === '--discard-prev') {
+      const prevElement = resultArray[i - 1];
+      if (prevElement && !controls.includes(prevElement)) {
+        resultArray[i - 1] = undefined;
+      }
+    } else if (arr[i] === '--double-next') {
+      if (i !== arr.length - 1) {
+        resultArray.splice(i, 2, arr[i+1], arr[i+1]);
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (i !== 0 && resultArray[i-1]) {
+        resultArray.splice(i - 1, 2, arr[i-1], arr[i-1]);
+      }
+    }
+  }
+
+  resultArray = resultArray.filter(el => el && !controls.includes(el));
+
+  return resultArray;
 }
 
 module.exports = {
